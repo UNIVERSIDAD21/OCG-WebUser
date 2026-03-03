77fc7413070bcef0019a413b79d3c20a33e62235
Borlty <efuentes1@udi.edu.co>
2026-03-03 21:15:58 +0000

    conectar configuración real de firebase para login y registro

--- ocg_proyect/lib/firebase_options.dart
+++ ocg_proyect/lib/firebase_options.dart
@@ -2,18 +2,8 @@
 // ignore_for_file: type=lint
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+    show TargetPlatform, defaultTargetPlatform, kIsWeb;
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
