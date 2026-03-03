85a90e34972e330cd5deeda62183973315f18d0a
Erik Fuentes <efuentes1@udi.edu.co>
2026-03-03 16:45:31 -0500

    firebase up

--- ocg_proyect/lib/firebase_options.dart
+++ ocg_proyect/lib/firebase_options.dart
@@ -37,13 +37,13 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAMmJh3fXAsLfe1XVUA6kGm9kEHHuj5iRY',
-    appId: '1:196011124818:web:c526108c8a981a71d7d78b',
-    messagingSenderId: '196011124818',
-    projectId: 'ocg-humanbionics',
-    authDomain: 'ocg-humanbionics.firebaseapp.com',
-    storageBucket: 'ocg-humanbionics.firebasestorage.app',
-    measurementId: 'G-9WZ1GY1YZR',
+    apiKey: "AIzaSyAMmJh3fXAsLfe1XVUA6kGm9kEHHuj5iRY",
+    authDomain: "ocg-humanbionics.firebaseapp.com",
+    projectId: "ocg-humanbionics",
+    storageBucket: "ocg-humanbionics.firebasestorage.app",
+    messagingSenderId: "196011124818",
+    appId: "1:196011124818:web:c526108c8a981a71d7d78b",
+    measurementId: "G-9WZ1GY1YZR",
   );
 
   static const FirebaseOptions android = FirebaseOptions(
